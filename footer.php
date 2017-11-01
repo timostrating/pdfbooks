4058ea222345fd3fac2b84f3c7c88790d2743f89
Fjjersak <32954694+Fjjersak@users.noreply.github.com>
2017-11-01 12:19:19 +0100

    footer aangepast

--- footer.php
+++ footer.php
@@ -1,6 +1,19 @@
-	
-<footer>
-	  Dit is de footer en dit werkt
+<style>
+.footer {
+   position: fixed;
+   font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #f9f9f9;
+   color: #333;
+   border-style: solid;
+   border-width: 1px;
+   border-color: #e7e7e7;
+   text-align: center;
+</style>
+<div class="footer">
+    <a href="contact.php">Contact</a>
 	</footer>
 </body>
 </html>
