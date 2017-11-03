1198c7b350230f885fd71332a594f773e6780ca5
Timo Strating <timostrating@home.nl>
2017-11-03 01:09:30 +0100

    commented the generators

--- noframework.php
+++ noframework.php
@@ -1,18 +1,44 @@
 #!/usr/bin/php
+
+                *****************
+                ** NOFRAMEWORK **
+                *****************
+ 
+                       NO
+   ______
+  / ____/___  ____ ___  ____  ____  ________  _____
+ / /   / __ \/ __ `__ \/ __ \/ __ \/ ___/ _ \/ ___/
+/ /___/ /_/ / / / / / / /_/ / /_/ (__  )  __/ /
+\____/\____/_/ /_/ /_/ .___/\____/____/\___/_/
+                    /_/
+
+                  JUST PURE PHP
+
 <?php
 
-generateScaffold("product");
+/**
+ * This script is used te generate code for you.
+ *   -  We have generate... functions. These create files for you.
+ *   -  We have add... functions. These alter currently existing files.
+ */
 
 
 
+generateScaffold("page");
+echo("\n");
+
+
+// Create the full MVC package
 function generateScaffold($name) {
+    // echo($name);
     generateModel($name);
     generateController($name);
     generateViews($name);
-    addRoute($name);
+    addRoutes($name);
 }
 
 
+// Create a new class in the /app/models folder.
 function generateModel($name) {
     $file = file_get_contents(__DIR__."/noframework/generateModel.txt.php");   
     $string = eval("return \"".$file."\";");  // pure meta programming right here
@@ -20,6 +46,7 @@ function generateModel($name) {
 }
 
 
+// Create a new Controller width the INDEX, SHOW, NEW, EDIT functions in the /app/models folder.
 function generateController($name) {
     $file = file_get_contents(__DIR__."/noframework/generateController.txt.php");
     $string = eval("return \"".$file."\";");  // pure meta programming right here
@@ -27,6 +54,7 @@ function generateController($name) {
 }
 
 
+// Create new INDEX, SHOW, NEW, EDIT views in a sepparate folder in /app/views.
 function generateViews($name) {
     mkdir(__DIR__."/app/views/".$name, 0755);
 
@@ -44,7 +72,8 @@ function generateViews($name) {
 }
 
 
-function addRoute($name) {  // TODO: as soon as the resource is implemented in the router we can simply this 
+// Add 4 new routes to our /config/routes.php file 
+function addRoutes($name) {  // TODO: as soon as the resource is implemented in the router we can simply this 
     $string = "
 // Crud
 \$router->add('/$name/index', '".ucfirst($name)."Controller#index');
@@ -56,7 +85,9 @@ function addRoute($name) {  // TODO: as soon as the resource is implemented in t
     file_put_contents(__DIR__."/config/routes.php", $string, FILE_APPEND); 
 }
 
-function createFile($URI, $string) {
+
+// Create a new file at the given $URI that has the text from $text inside of it
+function createFile($URI, $text) {
     echo "\t + ".$URI." \n";    
-    file_put_contents(__DIR__.$URI, $string);     
+    file_put_contents(__DIR__.$URI, $text);     
 }
