23389dda1e43ee7d96de3f66539f6146d8e85329
Javier Lo-Sie-Sen <32927686+javierlosiesen@users.noreply.github.com>
2017-11-06 13:01:15 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,6 +1,7 @@
 
 <html>
-    <title>Kapot</title>
+    <title>Page doesn't exist</title>
+    <h1>Oops, the link is broken.</h1>
         <a href="home.php"><img src="assets/helaas.png" style="width: 100%; height: 100%;"></a>
 
 </html>
