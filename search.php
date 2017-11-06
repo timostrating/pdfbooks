0f53ca48e2a374159471ac8a39c4b7ab67711474
Yaimon <yaimon@yaimons-imac.home>
2017-11-06 13:46:51 +0100

    search goeie

--- search.php
+++ search.php
@@ -9,21 +9,17 @@
 
     // Check if connection display any errors
     if ($db->connect_error) {
-        die("YOLO");
+        die("Connection failed");
     }
-      /*  mysqli_query($db, "CREATE TABLE Products (
-    ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR( 100 ) NOT NULL,
-    description VARCHAR( 255 ) NOT NULL, 
-    imgurl VARCHAR( 255 ) NOT NULL, 
-    price VARCHAR( 255 ) NOT NULL);");
-        mysqli_query($db, "INSERT INTO products (name, description, imgurl, price) VALUES 
-    ('Kokosnoten zijn geen specerijen', 'Een van de grootste boeken ooit', 'http://via.placeholder.com/550x350?text=Kokosnoten', '12.50'),
-    ('Piraten en de 7 dwergen', 'Een spanend avontuur', 'http://via.placeholder.com/350x550?text=Piraten', '5.00'),
-    ('Nederlands', 'Een veel gebruikt boek voor het vak Nederlands', 'https://placeimg.com/500/480?text=Nederlands', '7.25'),
-    ('Engels', 'Een veel gebruikt boek voor het vak Engels', 'https://placeimg.com/700/450?text=Engels', '8,99'),
-    ('Project management', 'Een veel gebruikt boek voor het vak project management', 'https://placeimg.com/600/600?text=Project+managment', '30,00')"); */
     
+    
+//  Products
+//     ID 
+//     name
+//     description 
+//     imgurl 
+//     price1
+
 
     if(isset($_POST['search'])){
         $search_value = $_POST['search'];
