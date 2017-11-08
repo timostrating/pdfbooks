29117bf9d457edcf542456594b1f8d4c82ea5e43
yaimon <yaimon@live.nl>
2017-11-08 16:27:28 +0100

    Admin pagina toegevoegd

---
+++ sidebarhead.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="admin.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<div class="area">
+    <div class="container">
+    
