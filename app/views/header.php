8a1e87869f408c3d7c007b1b714fd962c0df6248
Fjjersak <32954694+Fjjersak@users.noreply.github.com>
2017-11-09 12:43:52 +0100

    verwijder functie

--- app/views/header.php
+++ app/views/header.php
@@ -1,28 +1,33 @@
 <!doctype html>
 <html>
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title> pdfbooks</title>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <title> pdfbooks</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="<?= ROOT_PATH ?>/assets/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="<?= ROOT_PATH ?>/assets/style.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <script> <?php // TODO: add vanilla javascript support   // TODO: add fallback for when javascript is blocked ?>
-      (function($) {
+        <script> <?php // TODO: add vanilla javascript support   // TODO: add fallback for when javascript is blocked  ?>
+    (function ($) {
         $(document).ready(function () {
-          $('a[method="post"]').on('click', function(event) {
-            event.preventDefault();
-            $.post(this);
-            location.reload();
-          });
+            $('a[method="post"]').on('click', function (event) {
+                event.preventDefault();
+                $.post(this);
+                location.reload();
+            });
         });
-      })(jQuery)
-    </script> 
+    })(jQuery)
+    function AlertIt() {
+        var answer = confirm("Weet je zeker dat je je account wilt verwijderen?")
+        if (answer)
+            window.location = "delete.php";
+    }
+        </script> 
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  </head> <!-- head -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head> <!-- head -->
 
 
-  <body>
+    <body>
